caf4805d5e22e91fcd695fc78f5e995831c23f7e
Alexey <topychkanov123@yandex.ru>
2026-03-25 16:25:51 +0300

    510

---
+++ tasks/24/510.py
@@ -0,0 +1,9 @@
+import re
+from tqdm import tqdm
+
+text = open(r"C:\Users\aatop\Downloads\510_1.txt").read()
+
+num = r'(?:0|[1-9][0-9]*)'
+zero_prod = rf'(?:{num}\*)*0(?:\*{num})*'
+m = re.findall(rf'(?:{zero_prod}\+)+{zero_prod}',text)
+print(len(max(m,key=len)))
