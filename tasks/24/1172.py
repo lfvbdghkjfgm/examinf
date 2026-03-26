8095e40495ffa5446d3f2f4452d398ea3a18daba
Alexey <topychkanov123@yandex.ru>
2026-03-26 10:19:31 +0300

    1172

---
+++ tasks/24/1172.py
@@ -0,0 +1,13 @@
+import re
+
+text = open(r"C:\Users\aatop\Downloads\1172_1.txt").read()
+
+num = r'[1-9][0-9]*'
+m = re.findall(rf'(?:{num}[+*])+{num}',text)
+
+mx = 0
+for i in m:
+    i = i.replace('*','+')
+    i = i.split('+')
+    mx = max(mx,len(i))
+print(mx)
