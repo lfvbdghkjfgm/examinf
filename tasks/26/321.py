cdc5a19019f1cb9af3f2f58bc18bd75912b21560
Alexey <topychkanov123@yandex.ru>
2026-04-07 16:21:06 +0300

    321,330

---
+++ tasks/26/321.py
@@ -0,0 +1,22 @@
+
+
+data = [i.split() for i in open('1.txt')]
+data = [[int(a), int(b), c[1:]] for a,b,c in data]
+
+picksels = {}
+res = {}
+
+for a in range(1,10_000):
+    picksels[a] = [''] * 10_000
+    res[a] = 0
+
+for a,b,c in data:
+    picksels[a][b-1] = c
+
+for a,b in picksels.items():
+    if b.count('0000FF') < 6:
+        continue
+    for i in range(len(b)):
+        if i >= 3 and b[i] == '00FF00' and all([b[i-j] == '0000FF' for j in range(-3,0)]) and all([b[i-j] == '0000FF' for j in range(1,4)]):
+            res[a] += 1
+print(sum(res.values()),max([i for i in res if res[i] == max(res.values())]))
---
+++ tasks/26/330.py
@@ -0,0 +1,26 @@
+k = 10
+data = [list(map(int,i.split())) for i in open('1.txt')]
+data.sort()
+
+lines = [[0]*k]
+
+for a,b in data:
+    fl = False
+    for i in range(len(lines)):
+        for j in range(k):
+            if lines[i][j] < a:
+                lines[i][j] = b
+                fl = True
+                break
+        if fl:
+            break
+    if not fl:
+        lines.append([0]*k)
+        lines[-1][0] = b
+    ct = 0
+    for line in lines:
+        for home in line:
+            if home>a:
+                ct +=1
+    
+print(len(lines),ct)
