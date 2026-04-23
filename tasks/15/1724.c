4ea2b34388bd6ec0bd031e76d5e7484837d1c0b0
Alexey <topychkanov123@yandex.ru>
2026-04-23 10:18:11 +0300

    1724

---
+++ tasks/15/1724.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    for (int a = 0; a < 131072; a ++) {
+        int fl = 1;
+        for (int x = 1; x <= 131072; x ++) {
+            for (int y = 1; y <= 131072; y ++) {
+                if (!((131072 != 2 * y + 8 * x) || (a > 2 * x) && (a > y))) {
+                    fl = 0;
+                    break;
+                }
+                if (2* y + 8 * x > 131072) {
+                    break;
+                }
+            }
+            if (fl == 0) {
+                break;
+            }
+        }
+        if (fl) {
+            printf("%d",a);
+            break;
+        }
+    }
+
+    return 0;
+}
