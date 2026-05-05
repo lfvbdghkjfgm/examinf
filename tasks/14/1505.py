e6859791f0042e91f6a101688ef1e1d93bc8c0f4
Alexey <topychkanov123@yandex.ru>
2026-05-05 13:44:22 +0300

    1505

---
+++ tasks/14/1505.py
@@ -0,0 +1,14 @@
+def to_25(num):
+    res = []
+    while num:
+        res.append(num % 25)
+        num //= 25
+    return res[::-1]
+
+
+mn = [0, 0]
+for i in range(1, 2501):
+    t = to_25(25**150 + 25**100 - i)
+    if t.count(0) > mn[0]:
+        mn = [t.count(0), i]
+print(mn)
