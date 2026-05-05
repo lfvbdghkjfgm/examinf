0a5e43dc25b128df0ca0a7b089a5168eb832d670
lfvbdghkjfgm <topychkanov123@yandex.ru>
2026-05-05 18:43:24 +0300

    1483.py

---
+++ tasks/24/1483.py
@@ -0,0 +1,8 @@
+import re
+
+text = open(r"C:\Users\111\Downloads\1483_1.txt").read()
+text = text.replace("CDE", "#")
+
+m = re.findall(r"(?=((?:#[^#]){86}#[^E]))", text)
+mx = max(m, key=len)
+print(len(mx) + 87 * 2)
