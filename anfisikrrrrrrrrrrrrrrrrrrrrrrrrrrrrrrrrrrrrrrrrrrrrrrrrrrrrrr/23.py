60f75dfb97ad4afe6315969b412799cf3b1bc2cc
lfvbdghkjfgm <topychkanov123@yandex.ru>
2026-05-14 21:02:43 +0300

    remove anfisikrrr

--- anfisikrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr/23.py
+++
@@ -1,8 +0,0 @@
-def f(x, y):
-    if x > y:
-        return 0
-    if x == y:
-        return 1
-    else:
-        return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
-print(f(6, 14) * f(14, 18) * f(18, 48))
